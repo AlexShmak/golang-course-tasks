3ee446bf393205578adea6f259a4ac3c82c90cea
AlexShmak <lexnder9@gmail.com>
2025-08-27 22:20:50 +0300

    fix(l1.23): Make delete function polymorphic

--- level_1/l1.23/l1.23.go
+++ level_1/l1.23/l1.23.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
-func deleteSliceElByIndex(i int, s []int) []int {
-	copy(s[i:], s[i+1:])
+func deleteSliceElByIndex[T any](i int, s []T) []T {
 	n := len(s)
-	return s[:n-1]
+	if i < 0 || i >= n {
+		return s
+	}
+	return append(s[:i], s[i+1:]...)
 }
 
 func main() {
-	testSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(testSlice)
+	intSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(intSlice, len(intSlice))
 
-	testSlice = deleteSliceElByIndex(5, testSlice) // delete number at index 5 (number 6)
-	fmt.Println(testSlice)
+	intSlice = deleteSliceElByIndex(5, intSlice) // delete number at index 5 (number 6)
+	fmt.Println(intSlice, len(intSlice))
+
+	stringSlice := []string{"foo", "bar", "foobar"}
+	fmt.Println(stringSlice, len(stringSlice))
+
+	stringSlice = deleteSliceElByIndex(2, stringSlice) // delete string at index 5 ("foobar")
+	fmt.Println(stringSlice, len(stringSlice))
 }
